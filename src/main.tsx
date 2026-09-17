194553f4a1dcdb5fb7aeadd09c8d03f6493a0cf2
Yayasan Bina Tandang Nurul Falah <simsppgdarmaraja@gmail.com>
2026-09-17 17:04:31 +0700

    stage11: wire design tokens and root error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { AppErrorBoundary } from './components/layout/AppErrorBoundary'
 import './styles.css'
 import './brand.css'
+import './styles/tokens.css'
 import './design-system.css'
+import './styles/components.css'
 import './mobile-v5.css'
 
 const brandLogoUrl = new URL('logo-ra-nurul-falah.png', document.baseURI).toString()
@@ -18,8 +21,10 @@ if ('serviceWorker' in navigator && import.meta.env.PROD) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <AppErrorBoundary>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AppErrorBoundary>
   </StrictMode>,
 )
